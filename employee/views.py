d65bc5ff080d616789b274bae685479f7e00a917
cloner174 <hamedhajipour1400@gmail.com>
2024-11-16 00:56:18 +0330

    Apply Decorators

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from core.decorators import group_required
+
+@group_required('Employee')
+def dashboard(request):
+    return render(request, 'employee/dashboard.html')

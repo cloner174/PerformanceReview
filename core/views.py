66c64d1628daca0fb61bc9801b3a03ff9f33060d
cloner174 <hamedhajipour1400@gmail.com>
2024-11-16 00:50:04 +0330

    Login View

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
+
+
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'core/login.html'
+    
+
+
+@login_required
+def redirect_view(request):
+    if request.user.groups.filter(name='Senior Manager').exists():
+        return redirect('senior_manager:dashboard')
+    elif request.user.groups.filter(name='Manager').exists():
+        return redirect('manager:dashboard')
+    elif request.user.groups.filter(name='Employee').exists():
+        return redirect('employee:dashboard')
+    else:
+        return redirect('login')
+
+#cloner174

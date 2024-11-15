2953378446baaea6aabc97f007fe655ad92b8884
cloner174 <hamedhajipour1400@gmail.com>
2024-11-16 01:36:28 +0330

    add statics

--- performance_review/settings.py
+++ performance_review/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/redirect/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]

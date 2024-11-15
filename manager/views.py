71d7f3707cd3aab46186ee10d41cc6b360b24e44
cloner174 <hamedhajipour1400@gmail.com>
2024-11-16 00:54:09 +0330

    Apply Decorators

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from core.decorators import group_required
+
+@group_required('Manager')
+def dashboard(request):
+    return render(request, 'manager/dashboard.html')
+
+#cloner174

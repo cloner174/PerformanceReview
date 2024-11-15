e0c2bd307df3fb99063541366468c60e0cf5f6be
cloner174 <hamedhajipour1400@gmail.com>
2024-11-16 00:57:47 +0330

    Apply Decorators

--- employee/views.py
+++ employee/views.py
@@ -7,3 +7,5 @@ from core.decorators import group_required
 @group_required('Employee')
 def dashboard(request):
     return render(request, 'employee/dashboard.html')
+
+#cloner174
--- senior_manager/views.py
+++ senior_manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from core.decorators import group_required
+
+@group_required('Senior Manager')
+def dashboard(request):
+    return render(request, 'senior_manager/dashboard.html')
+
+#cloner174
